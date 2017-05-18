0a17028264bc5ca654d91d83ea2ca1c714b99507
chain24 <chainsy97@gmail.com>
2017-05-18 10:41:59 +0800

    rm some url

--- vendor/laravel/lumen-framework/config/constant.php
+++ vendor/laravel/lumen-framework/config/constant.php
@@ -6,7 +6,7 @@
  * Time: 14:11
  */
 return [
-    'APP_IMG_DOMAIN' => 'http://www.superbiiz.com',
+    'APP_IMG_DOMAIN' => 'xxxxxxxxxxx',
     'APP_IMG_DEFAULT' => '/images/notavailable.jpg',
     'APP_IMG_DOMAIN_FOLDER' => '/newg',
     'ADD_PROCESS_TIME' => array(10,19),
